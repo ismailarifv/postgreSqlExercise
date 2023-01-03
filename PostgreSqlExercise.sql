fb0feb633b5f740412acc8da9662007eb0322366
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-03 13:15:50 +0300

    Postgre

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -79,3 +79,31 @@ WHERE title ILIKE '%t%t%t%t%t%';
 --4.Soru
 SELECT * FROM film
 WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+
+/*
+ÖDEV4
+1-film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+2-film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+3-film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye 
+eşittir?
+4-country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+5-city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+*/
+
+--1.Soru
+SELECT DISTINCT replacement_cost FROM film;
+
+--2.Soru
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+--3.Soru
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+--4.Soru
+SELECT COUNT(*) FROM country
+WHERE country LIKE '_____';
+
+--5.Soru
+SELECT COUNT(*) FROM city
+WHERE city ILIKE '%r';
