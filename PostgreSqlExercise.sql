9c9b27108b08999a86a451018794f4d9a54a478e
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-03 12:49:15 +0300

    Postgre

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -51,3 +51,31 @@ WHERE first_name IN('Penelope', 'Nick', 'Ed');
 --3.Soru
 SELECT * FROM film
 WHERE rental_rate IN(0.99,2.99,4.99) AND replacement_cost IN(12.99,15.99,28.99);
+
+/*
+ÖDEV3
+1-country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile 
+sonlananları sıralayınız.
+2-country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile
+ sonlananları sıralayınız.
+3-film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' 
+karakteri içeren film isimlerini sıralayınız.
+4-film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük
+olan ve rental_rate 2.99 olan verileri sıralayınız.
+*/
+
+--1.Soru
+SELECT country FROM country
+WHERE country ~~ 'A%a';
+
+--2.Soru
+SELECT country FROM country
+WHERE country LIKE '%_____n';
+
+--3.Soru
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%t%';
+
+--4.Soru
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
