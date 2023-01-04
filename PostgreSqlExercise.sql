4e9b99aeb234e054a35fc6b2c103f6f2d7157036
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-04 18:43:33 +0300

    PostgreSql

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -107,3 +107,32 @@ WHERE country LIKE '_____';
 --5.Soru
 SELECT COUNT(*) FROM city
 WHERE city ILIKE '%r';
+
+
+/*
+ÖDEV5
+1-film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+2-film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 
+5 filmi(6,7,8,9,10) sıralayınız.
+3-customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi 
+sıralayınız.
+*/
+
+--1.Soru
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+--2.Soru
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length 
+OFFSET 5
+LIMIT 5;
+
+--3.Soru
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
