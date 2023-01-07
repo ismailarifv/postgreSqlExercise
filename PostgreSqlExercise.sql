18964cc9830837358676b81f5abcaf4eaf308ca3
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-07 14:50:49 +0300

    Exercise_7

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -159,3 +159,32 @@ WHERE rental_rate = 0.99;
 --4.Soru
 SELECT COUNT(DISTINCT(replacement_cost)) FROM film
 WHERE length > 150;
+
+/* 
+Ödev7
+1-film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+2-film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan 
+replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+3-customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+4-City tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı 
+barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+*/
+
+--1.Soru
+SELECT rating FROM film
+GROUP BY rating ;
+
+--2.Soru
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+--3.Soru
+SELECT store_id , COUNT(*) FROM customer
+GROUP BY store_id;
+
+--4.Soru
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
