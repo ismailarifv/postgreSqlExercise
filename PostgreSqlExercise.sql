d0fd501a7fcdcb5f00268dcc24286cf199c031f8
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-09 11:33:53 +0300

    Exercise_8

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -188,3 +188,41 @@ SELECT country_id, COUNT(*) FROM city
 GROUP BY country_id
 ORDER BY COUNT(*) DESC
 LIMIT 1;
+
+/*
+Ödev8
+1-test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) 
+olan bir tablo oluşturalım.
+2-Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+3-Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+4-Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+*/
+--1.Soru
+CREATE TABLE employee (
+    id INTEGER,
+    name VARCHAR(50),
+    birthday DATE,
+    email VARCHAR(100)
+)
+--2.Soru
+--EKLENDİ
+
+--3.Soru
+UPDATE employee
+SET name ='İsmail',
+    birthday ='1998-11-01',
+    email = 'i.arifgulec8110@gmail.com'
+WHERE id=1 ;
+UPDATE employee
+SET name = 'Eslem',
+    birthday = '1999-11-22',
+    email = 'eslem_tifi@outllok.com'
+WHERE name LIKE 'A%';
+
+--4.Soru
+DELETE FROM employee
+WHERE name = 'Eslem';
+DELETE FROM employee
+WHERE id = 1;
+DELETE FROM employee
+WHERE name LIKE 'C%';
