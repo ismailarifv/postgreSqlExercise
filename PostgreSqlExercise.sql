34f505544ff113b8a6d92b622d51c912578e188c
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-10 13:01:19 +0300

    Exercise_10

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -248,3 +248,25 @@ INNER JOIN payment ON customer.customer_id = payment.customer_id;
 --3.Soru
 Select first_name,last_name,rental_id from rental
 INNER JOIN customer ON customer.customer_id = rental.customer_id;
+
+/* 
+Ödev10
+1-city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN
+sorgusunu yazınız.
+2-customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini 
+birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+3-customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini 
+birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
+*/
+--1.Soru
+SELECT city,country FROM city
+LEFT JOIN country ON city.country_id = country.country_id; 
+
+--2.Soru
+SELECT first_name,last_name,payment_id FROM customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id; 
+
+--3.Soru
+Select first_name,last_name,rental_id from customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
