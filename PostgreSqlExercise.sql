47264d35254c770cd2ef348ce71c7fb42c6e7d50
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-10 13:38:26 +0300

    Exercise_11

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -270,3 +270,85 @@ RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
 --3.Soru
 Select first_name,last_name,rental_id from customer
 FULL JOIN rental ON customer.customer_id = rental.customer_id;
+
+/*
+Ödev11
+1-actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+2-actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+3-actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+4-İlk 3 sorguyu tekrar eden veriler için de yapalım.
+
+*/
+
+--1.Soru
+
+(
+    SELECT first_name FROM actor
+    
+)
+UNION
+(
+    SELECT first_name FROM customer
+    
+)
+ORDER BY first_name;
+
+--2.Soru
+(
+    SELECT first_name FROM actor
+    
+)
+INTERSECT
+(
+    SELECT first_name FROM customer
+    
+)
+ORDER BY first_name;
+
+--3.Soru
+(
+    SELECT first_name FROM actor
+    
+)
+EXCEPT 
+(
+    SELECT first_name FROM customer
+    
+)
+ORDER BY first_name;
+
+--4.Soru
+    --1.Madde
+    (
+    SELECT first_name FROM actor
+    
+    )
+    UNION ALL
+    (
+    SELECT first_name FROM customer
+    
+    )
+    ORDER BY first_name;
+    --2.Madde
+    (
+    SELECT first_name FROM actor
+    
+    )
+    INTERSECT
+    (
+        SELECT first_name FROM customer
+        
+    )
+    ORDER BY first_name;
+    --3.Madde
+    (
+    SELECT first_name FROM actor
+    
+    )
+    EXCEPT ALL
+    (
+        SELECT first_name FROM customer
+        
+    )
+    ORDER BY first_name;
+
