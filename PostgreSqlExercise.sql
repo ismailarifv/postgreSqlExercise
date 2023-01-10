9781906d48f4d8bbf8cc16027a0934cc88eae206
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-01-10 20:22:19 +0300

    Exercise_12

--- PostgreSqlExercise.sql
+++ PostgreSqlExercise.sql
@@ -352,3 +352,40 @@ ORDER BY first_name;
     )
     ORDER BY first_name;
 
+/*
+Ödev12
+1-film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane
+film vardır?
+2-film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+3-film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+4-payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+*/
+
+--1.Soru
+SELECT COUNT(length) FROM film
+WHERE length >
+(SELECT AVG(length) FROM film);
+
+--2.Soru
+SELECT COUNT(rental_rate) FROM film
+WHERE rental_rate = 
+(SELECT MAX(rental_rate) FROM film);
+
+--3.Soru
+SELECT rental_rate,replacement_cost FROM film
+WHERE rental_rate =
+(SELECT MIN(rental_rate) FROM film
+) 
+AND 
+replacement_cost = 
+(SELECT MIN(replacement_cost) FROM film
+) 
+
+--4.Soru
+SELECT DISTINCT customer_id ,
+(SELECT Count(*) FROM payment WHERE customer_id = main_payment.customer_id ) AS payment_count
+FROM payment AS main_payment 
+ORDER BY payment_count DESC
+LIMIT 10;
+
